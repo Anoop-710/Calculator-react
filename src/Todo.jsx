9d19d71a3bc825589d024d99abc03f283ae55736
anoop <anoopaathreya@gmail.com>
2023-01-20 12:50:13 +0530

    Todo completed

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 
 const Todo = () => {
-    const [items, setItems] = React.useState([{text: 'hello', done: false}]);
+    const [items, setItems] = React.useState([{text: 'hello', done: false, editing: false}]);
 
    
 
     const handleSubmit = (e) =>{
         e.preventDefault();
         const todo = (e.target.elements.todo.value);
-        setItems([...items, {text: todo, done: false}]);
+        setItems([...items, {text: todo, done: false, editing: false}]);
         // console.log(e.target.elements.todo.value);
         e.target.elements.todo.value = "";
         
@@ -25,8 +25,20 @@ const Todo = () => {
         updatedToDos[index].done = !updatedToDos[index].done;
         setItems(updatedToDos);
     }
+
+    const  handleEdit = (index, event) =>{
+        const updatedToDos = [...items];
+        updatedToDos[index].text = event.target.value;
+        setItems(updatedToDos);
+    }
+
+    const handleEditButton = (index, event) => {
+        const updatedToDos = [...items];
+        updatedToDos[index].editing = !updatedToDos[index].editing;
+        setItems(updatedToDos);
+    }
   return (
-    <div style={{padding: '25%', position:'fixed'}}>
+    <div  style={{padding: '25%'}}>
         <h1 className='display-1 text-center'>Todo List</h1>
         <form className='d-flex gap-4' onSubmit={handleSubmit}>
             <input type="text" name='todo' placeholder='Enter to-do text' className='form-control w-72'  />
@@ -37,11 +49,17 @@ const Todo = () => {
             <ul className='list-group'>
                 {items.map((item,i)=>
                     <li key={i} className="list-group-item">
+                        {item.editing?<input className='form-control' value={item.text} onChange={(e) => handleEdit(i,e)}/>:
+                        
                         <div style={{float: "left", textDecoration: item.done? 'line-through': 'none'}}>
                         {item.text}
                         </div>
+
+                        }
+                        
                         <button style={{float: "right"}} className={'btn ' + (item.done? 'btn-secondary': 'btn-success')} onClick={()=>handleDone(i)}>{item.done?'undo':'done'}</button>
-                        <button style={{float: "right"}} className="btn btn-danger mx-2" onClick={handleDelete}>Delete</button>
+                        <button style={{float: "right"}} className="btn btn-danger mx-2" onClick={()=>handleDelete(i)}>Delete</button>
+                        <button style={{float: "right"}} className="btn btn-warning mx-2" onClick={()=>handleEditButton(i)}>Edit</button>
                     </li>
                 )}
             </ul>
